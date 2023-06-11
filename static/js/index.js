5c119b1d4af074c6f744e5a7782d828aac00ae13
desng5 <desng5@gmail.com>
2023-06-11 03:08:15 -0700

    html edit

--- static/js/index.js
+++ static/js/index.js
@@ -1,127 +1,151 @@
-// Define the player class
-class Player {
-  constructor(name) {
-    this.name = name;
-    this.score = 0;
-  }
+document.addEventListener("DOMContentLoaded", function() {
+    const playerNames = [];
+    const playerScores = [];
+    let currentPlayer = 0;
 
-  rollDice() {
-    const dice = [];
-    for (let i = 0; i < 6; i++) {
-      dice.push(Math.floor(Math.random() * 6) + 1);
-    }
-    return dice;
-  }
+    const numPlayersInput = document.getElementById("num-players");
+    const playerNamesButton = document.getElementById("player-names-button");
+    const startGameButton = document.getElementById("start-game-button");
+    const nameInputsContainer = document.getElementById("name-inputs");
+    const rollButton = document.getElementById("roll-button");
+    const diceContainer = document.getElementById("dice-container");
+    const playerScoresContainer = document.getElementById("player-scores");
+    const gameMessages = document.getElementById("game-messages");
 
-  updateScore(points) {
-    this.score += points;
-  }
-}
-
-function hasStraight(dice) {
-  return dice.length === 6 && new Set(dice).size === 6;
-}
-
-function hasThreeOfKind(dice, value) {
-  const count = dice.filter((d) => d === value).length;
-  return count >= 3;
-}
-
-function calculateScore(dice) {
-  let score = 0;
-
-  if (hasStraight(dice)) {
-    score += 1500;
-  } else {
-    const counts = new Array(7).fill(0);
-
-    for (const value of dice) {
-      counts[value]++;
-    }
-
-    for (let i = 1; i <= 6; i++) {
-      if (counts[i] >= 3) {
-        if (i === 1) {
-          score += 1000;
-        } else {
-          score += i * 100;
+    playerNamesButton.addEventListener("click", function() {
+        const numPlayers = parseInt(numPlayersInput.value);
+        if (numPlayers < 1 || numPlayers > 4) {
+            alert("Please enter a number between 1 and 4 for the number of players.");
+            return;
         }
-        counts[i] -= 3;
-      }
 
-      if (i === 1 || i === 5) {
-        score += counts[i] * (i === 1 ? 100 : 50);
-      }
-    }
-  }
+        // Clear any existing name inputs
+        nameInputsContainer.innerHTML = "";
 
-  return score;
-}
+        // Create name inputs for each player
+        for (let i = 0; i < numPlayers; i++) {
+            const nameInput = document.createElement("input");
+            nameInput.type = "text";
+            nameInput.placeholder = "Player " + (i + 1) + " name";
+            nameInputsContainer.appendChild(nameInput);
+        }
 
-function addPlayer(name) {
-  const player = new Player(name);
-  players.push(player);
-  const playerElement = document.createElement('div');
-  playerElement.textContent = `${name}: 0`;
-  document.getElementById('players').appendChild(playerElement);
-}
-
-function updatePlayerScore(player) {
-  const playerElement = document.querySelector(`#players div:contains(${player.name})`);
-  playerElement.textContent = `${player.name}: ${player.score}`;2
-}
-
-function animateDice() {
-    const diceElements = document.querySelectorAll('.dice');
-    const rollButton = document.getElementById('rollButton');
-    rollButton.disabled = false;
-  
-    diceElements.forEach((diceElement) => {
-      diceElement.classList.add('rolling');
+        startGameButton.style.display = "block";
     });
-  
-    setTimeout(() => {
-      diceElements.forEach((diceElement) => {
-        diceElement.classList.remove('rolling');
-      });
-  
-      const dice = currentPlayer.rollDice();
-      const score = calculateScore(dice);
-      currentPlayer.updateScore(score);
-      updatePlayerScore(currentPlayer);
-  
-      dice.forEach((value, index) => {
-        const diceElement = diceElements[index];
-        diceElement.className = `dice dice${value}`;
-      });
-  
-      if (currentPlayer.score >= 10000) {
-        document.getElementById('game').textContent = `${currentPlayer.name} wins!`;
-        rollButton.disabled = false;
-        return;
-      }
-  
-      currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
-      currentPlayer = players[currentPlayerIndex];
-      rollButton.disabled = false;
-    }, 1000);
-  }
-  
 
-const players = [];
-let currentPlayerIndex = 0;
-let currentPlayer = null;
+    startGameButton.addEventListener("click", function() {
+        const nameInputs = nameInputsContainer.getElementsByTagName("input");
+        for (let i = 0; i < nameInputs.length; i++) {
+            const playerName = nameInputs[i].value.trim();
+            if (!playerName) {
+                alert("Please enter names for all players!");
+                return;
+            }
+            playerNames[i] = playerName;
+            playerScores[i] = 0;
+        }
 
-const numPlayers = parseInt(prompt('Enter the number of players (1-4):'), 10);
+        // Hide input fields and buttons
+        numPlayersInput.style.display = "none";
+        playerNamesButton.style.display = "none";
+        nameInputsContainer.style.display = "none";
+        startGameButton.style.display = "none";
 
-if (isNaN(numPlayers) || numPlayers < 1 || numPlayers > 4) {
-  alert('Invalid number of players. Please refresh the page to try again.');
-} else {
-  for (let i = 1; i <= numPlayers; i++) {
-    const playerName = prompt(`Enter the name of Player ${i}:`);
-    addPlayer(playerName);
-  }
+        // Show game elements
+        diceContainer.style.display = "block";
+        playerScoresContainer.style.display = "block";
+        rollButton.style.display = "block";
 
-  currentPlayer = players[currentPlayerIndex];
-  document.getElementById('rollButton').addEventListener('click', animateDice);
-}
+        updatePlayerScoresUI();
+    });
+
+    rollButton.addEventListener("click", function() {
+        const dice = rollDice();
+        const score = calculateScore(dice);
+
+        playerScores[currentPlayer] += score;
+
+        // Update UI
+        updateDiceUI(dice);
+        updatePlayerScoresUI();
+
+        if (playerScores[currentPlayer] >= 10000) {
+            gameMessages.textContent = playerNames[currentPlayer] + " wins the game!";
+            rollButton.disabled = true;
+            return;
+        }
+
+        currentPlayer = (currentPlayer + 1) % playerNames.length;
+    });
+
+    function rollDice() {
+        const dice = [];
+        for (let i = 0; i < 6; i++) {
+            dice.push(Math.floor(Math.random() * 6) + 1);
+        }
+        return dice;
+    }
+
+    function calculateScore(dice) {
+        const count = Array.from({ length: 7 }, () => 0);
+        let score = 0;
+
+        for (const value of dice) {
+            count[value]++;
+        }
+
+        // Single 5's
+        score += count[5] * 50;
+
+        // Single 1's
+        score += count[1] * 100;
+
+        // Three of a Kind
+        for (let i = 1; i <= 6; i++) {
+            if (count[i] >= 3) {
+                if (i === 1) {
+                    score += 1000;
+                } else {
+                    score += i * 100;
+                }
+                count[i] -= 3;
+            }
+        }
+
+        // Three of a Kind of 4 or 5
+        if (count[4] >= 3) {
+            score += 400;
+            count[4] -= 3;
+        }
+        if (count[5] >= 3) {
+            score += 500;
+            count[5] -= 3;
+        }
+
+        // Straight
+        if (count.slice(1).every(val => val === 1)) {
+            score += 1500;
+        }
+
+        return score;
+    }
+
+    function updateDiceUI(dice) {
+        diceContainer.innerHTML = "";
+        for (const value of dice) {
+            const diceElement = document.createElement("div");
+            diceElement.classList.add("dice");
+            diceElement.style.backgroundImage = "url('images/dice" + value + ".png')";
+            diceContainer.appendChild(diceElement);
+        }
+    }
+
+    function updatePlayerScoresUI() {
+        playerScoresContainer.innerHTML = "";
+        for (let i = 0; i < playerNames.length; i++) {
+            const scoreElement = document.createElement("div");
+            scoreElement.textContent = playerNames[i] + ": " + playerScores[i];
+            playerScoresContainer.appendChild(scoreElement);
+        }
+    }
+});
